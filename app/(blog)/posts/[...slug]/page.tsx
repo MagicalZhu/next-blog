1b99ead0551097f9ab8d07aa02ce2e2e106617c0
YuLiang Zhu <zhuyuliangm@outlook.com>
2024-02-20 08:27:51 +0000

    add ahooks

--- app/(blog)/posts/[...slug]/page.tsx
+++ app/(blog)/posts/[...slug]/page.tsx
@@ -69,7 +69,11 @@ export default async function PostPage({ params }: PostPageProps) {
   const toc = await getTableOfContents(page.body.raw)
   return (
     <>
-      <div className="slide-enter max-w-[68vw] mx-auto flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+      <div className={cn(
+        "slide-enter max-w-[75vw] mx-auto flex-1 items-start",
+        "md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6",
+        "lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10"
+      )}>
         <aside className="fixed top-14 z-30 hidden w-full shrink-0 md:sticky md:block">
           <ScrollArea className="h-full py-6 pr-6 lg:py-8">
             <TableOfContents toc={toc} />
--- components/layout/ScrollBtn.tsx
+++ components/layout/ScrollBtn.tsx
@@ -4,20 +4,28 @@ import { Button } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import {  cn } from "@/lib/utils"
 
+import React from 'react';
+import { useScroll } from 'ahooks';
+
 export function ScrollBtn() {
+  const scroll = useScroll(document);
   return (
     <Button
       variant="ghost"
+      size="icon"
       title="Scroll to top"
       className={cn(
         "sticky right-6 bottom-3 rounded-full float-right",
+        scroll?.top && scroll?.top > 100
+          ? "opacity-80"
+          : "hidden"
       )}
       onClick = {() => window.scrollTo({
         top: 0,
         behavior: 'smooth',
       })}
     >
-      <Icons.ArrowUpIcon className="w-4 h-6" />
+      <Icons.ArrowUpIcon className="h-4 w-4" />
   </Button>
   )
 }
